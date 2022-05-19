7db0f8ad4f3f818dce1a1a817acc61aa1aed6474
Jesus Puentes <jopt98@gmail.com>
2022-05-19 14:56:23 -0500

    Updated ico

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <Head>
         <title>Jopt05 | Web Developer</title>
         <meta name="description" content="Jopt05 portfolio" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/imgs/loader.svg" />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;600&display=swap" rel="stylesheet" />
         <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet' />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
