1829e08b3cae2973743beb9db0c42cdf128a71ed
Jesus Puentes <jopt98@gmail.com>
2023-07-30 10:37:10 -0600

    Continua trabajo en el form

--- components/login/form/index.js
+++ components/login/form/index.js
@@ -9,7 +9,7 @@ export const Form = () => {
 
     function handleChange(e) {
         setForm({
-            ...Form,
+            ...form,
             [e.target.name]: e.target.value
         });
     };
@@ -25,16 +25,20 @@ export const Form = () => {
             `${url}/api/usuarios/login`,
             {
                 method: 'POST',
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(form)
-            }
+            },
         )
             .then((response) => response.json())
             .then((response) => {
+                console.log(response);
 
                 if (response.token) {
-                    setIsLoading(false);
-                    return;
-                };
+                    console.log("Cargado")
+                } else {
+                    setErrors(response.errors.map((element) => element.msg));
+                }
+                setIsLoading(false);
             })
             
             .catch((err) => console.log(err))
@@ -54,7 +58,7 @@ export const Form = () => {
                     <input 
                         name='user_name' 
                         className={styles.container__form_input} 
-                        type='email' 
+                        type='text' 
                         onChange={handleChange}
                     />
                 </div>
@@ -69,17 +73,20 @@ export const Form = () => {
                         onChange={handleChange}
                     />
                 </div>
-                <button className={styles.container__form_button}>
+                <button 
+                    className={`${styles.container__form_button} ${isLoading && styles.container__form_button_deactivated}`}
+                >
                     Enviar
                 </button>
                 <div className={styles.loaderdiv}>
                     <img 
-                        className={`${isLoading ? styles.loaderdiv__svg_activated : styles.loaderdiv__svg}`}
+                        className={`${styles.loaderdiv__svg} ${isLoading && styles.loaderdiv__svg_activated}`}
                         src='/images/imgs/loader.svg'
                     />
                 </div>
-                <div className={styles.errorsdiv}>
-                    {/* errorsdiv__activated */}
+                <div 
+                    className={`${styles.errorsdiv} ${errors.length > 0 && styles.errorsdiv__activated}`}
+                >
                     <p className={styles.errorsdiv__text}>
                         Error de prueba
                     </p>
