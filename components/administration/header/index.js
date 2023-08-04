be6dc71f7f7ec45e8bc64ed6c302beeeeb6ecced
Jesus Puentes <jopt98@gmail.com>
2023-08-04 12:25:31 -0600

    Se agregó un header

--- components/administration/header/index.js
+++ components/administration/header/index.js
@@ -6,19 +6,24 @@ export const Header = ({
     data
 }) => {
     return (
-        <div className={styles.body}>
-            <Metric 
-                data={data.projects.length}
-                name={'Proyects'}
-            />
-            <Metric 
-                data={data.services.length}
-                name={'Services'}
-            />
-            <Metric 
-                data={data.technologies.length}
-                name={'Technologies'}
-            />
-        </div>
+        <>
+            <div className={styles.header}>
+                <h1>Admin Panel</h1>
+            </div>
+            <div className={styles.body}>
+                <Metric 
+                    data={data.projects.length}
+                    name={'Proyects'}
+                />
+                <Metric 
+                    data={data.services.length}
+                    name={'Services'}
+                />
+                <Metric 
+                    data={data.technologies.length}
+                    name={'Technologies'}
+                />
+            </div>
+        </>
     )
 }
